b7a1576bbd981fbb91769d2e3478d6931c75e7ac
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-26 18:21:12 +0000

    Add list from an array dynamically

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import './style.css';
+// import './style.css';
 
-const container = document.querySelector('.list-div');
+const tasks = [
+  {
+    description: 'Pay the bills',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Pick up the kids',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Write some code',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Drink coffee',
+    completed: true,
+    index: 3,
+  },
+];
 
-console.log(container);
+const loadTask = (arr) => {
+  arr.forEach((task) => {
+    const container = document.querySelector('.list-div');
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <input type="checkbox" name="" id="">
+    <p class="task-to-be-done">${task.description}</p>
+    `;
+    container.appendChild(listItem);
+  });
+};
+
+loadTask(tasks);
